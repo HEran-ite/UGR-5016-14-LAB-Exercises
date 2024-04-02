f3ef08dc2a2beb89da2abad77033c6c0e4a0d928
HEran-ite <herieshetu@gmail.com>
2024-04-02 15:55:24 +0300

    Added new feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lab2/pages/page1.dart';
 import 'package:lab2/pages/page2.dart';
+import 'package:lab2/pages/page3.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-        home: page2(),
+        home: page3(),
       
     );
   }
