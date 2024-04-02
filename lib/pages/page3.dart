baf24bc59b63c2fb19a41a8ae3629ac87fb11430
HEran-ite <herieshetu@gmail.com>
2024-04-02 16:28:24 +0300

    Added new feature

--- lib/pages/page3.dart
+++ lib/pages/page3.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class page3 extends StatelessWidget {
@@ -5,6 +6,66 @@ class page3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+      
+        title: Text('BMI CALCULATOR',
+        style: TextStyle(color: Colors.white),),
+        backgroundColor: Colors.purple,
+        
+      ),
+      backgroundColor: Colors.blue,
+      body:Container(
+        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          
+          children: [
+     
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+            ElevatedButton(onPressed: (){}, child: Icon(Icons.male,size: 100,)
+            ,),
+            ElevatedButton(onPressed: (){}, child: Icon(Icons.female,size: 100,))
+          ],),
+          Column(children: [
+            Text('height'),
+            Text('176'),
+            Icon(Icons.add)
+
+          ],),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children:[ Column(children: [
+              Text('Weight'),
+            Text('60'),
+            Row(children: [
+              Icon(Icons.minimize),
+              Icon(Icons.add_circle)
+            ],),
+            
+            ],),
+            Column(children: [
+              Text('Weight'),
+            Text('60'),
+            Row(children: [
+              Icon(Icons.minimize),
+              Icon(Icons.add_circle)
+            ],),
+            
+            ],),
+          ])
+        ],),
+      ) ,
+      bottomNavigationBar: BottomAppBar(
+        
+        child: ElevatedButton(onPressed: (){},child: Text('CALCULATE',
+        style: TextStyle(color: Colors.purple,),
+        
+        
+        ),)
+      ),
+    );
   }
 }
